a0500bef923a3bd525b9a1970bbc1bb597c0953c
Rait Kapp <3442567+raigu@users.noreply.github.com>
2022-05-02 08:15:46 +0300

    Change alias

--- lib/LoremIpsumBundle/src/DependencyInjection/KnpULoremIpsumExtension.php
+++ lib/LoremIpsumBundle/src/DependencyInjection/KnpULoremIpsumExtension.php
@@ -11,8 +11,12 @@ final class KnpULoremIpsumExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.xml');
     }
 
+    public function getAlias()
+    {
+        return 'knpu_lorem_ipsum';
+    }
 }
